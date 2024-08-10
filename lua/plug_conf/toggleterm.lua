b33bb3697b66d2ea01d2efc6c1b22246dfd6aac6
defghij <defghij@arda>
2024-08-10 15:31:55 -0400

    term mapping

--- lua/plug_conf/toggleterm.lua
+++ lua/plug_conf/toggleterm.lua
@@ -2,9 +2,9 @@ local mode = require("consts").modes
 
 local Direction = {
   horizontal = "horizontal",
-  vertical = "vertical",
-  tab = "tab",
-  float = "float"
+  vertical   = "vertical",
+  tab        = "tab",
+  float      = "float"
 }
 
 require("toggleterm").setup {
@@ -13,14 +13,15 @@ require("toggleterm").setup {
   size = function(term)
     return math.floor(vim.api.nvim_win_get_height(0) * 0.5)
   end,
-  hide_numbers = true,
-  shade_terminals = false,
-  shading_factor = 3,
-  start_in_insert = true,
-  insert_mappings = true,
+  hide_numbers      = true,
+  shade_terminals   = false,
+  shading_factor    = 3,
+  start_in_insert   = true,
+  insert_mappings   = true,
   terminal_mappings = true,
-  persist_size = true,
-  close_on_exit = true,
+  persist_size      = true,
+  persist_mode      = true,
+  close_on_exit     = true,
   shell = vim.o.shell,
   float_opts = {
     border = 'curved',
@@ -34,5 +35,8 @@ require("toggleterm").setup {
   }
 }
 
-vim.keymap.set(mode.visual_select, "<leader>t", ":ToggleTermSendVisualSelection<CR>", { noremap = true })
+--vim.keymap.set(mode.visual_select, "<leader>T", ":ToggleTermSendVisualSelection<CR>", { noremap = true })
 vim.keymap.set(mode.normal,        "<leader>t", ":ToggleTerm<CR>", { noremap = true })
+vim.keymap.set(mode.normal,        "<leader>tF", ":ToggleTerm direction=float<CR>", { noremap = true })
+vim.keymap.set(mode.normal,        "<leader>tV", ":ToggleTerm> direction=vertical<CR>", { noremap = true })
+vim.keymap.set(mode.normal,        "<leader>tH", ":ToggleTerm> direction=horizontal<CR>", { noremap = true })
